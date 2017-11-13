2e58ce92ce8ba70ca41dc67ed84917bcf1cab347
Surendra <surendra.veerepalli@gmail.com>
2017-11-13 10:42:50 +0530

    New Project

---
+++ ClassDemo/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassDemo.Models;
+
+namespace ClassDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Application> _appList = new List<Application>();
+
+            public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationIndex()
+        {
+            return View(_appList);
+        }
+
+         
+        public ActionResult Form()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult Form(FormCollection frm)
+        {
+
+            var id = Int32.Parse(frm["id"]);
+            var name = frm["name"];
+            var age = Int32.Parse(frm["age"]);
+            var mobile = frm["mobile"];
+            var gender = Int32.Parse(frm["gender"]);
+
+            var salutation=string.Empty;
+            if (gender == 1)
+                salutation = "Miss.";
+            else
+                salutation = "Mr.";
+
+            _appList.Add(new Application { id = id, name = name, age = age, gender = gender, mobile = mobile });
+
+            //ViewBag.Message = salutation +" "+ name+ " Your application is submitted successful";
+            //return View();
+            return RedirectToAction("ApplicationIndex");
+        }
+
+
+    }
+}
---
+++ ClassDemo/Models/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassDemo.Models
+{
+    public class Application
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string mobile { get; set; }
+        public int gender { get; set; }
+
+
+    }
+}
