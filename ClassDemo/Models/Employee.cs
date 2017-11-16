adade577d436a7e52d54489207935b4beef95ffb
manidevanaboina <mani.devanaboina@gmail.com>
2017-11-16 10:05:57 +0530

    Employee Details creation, view

---
+++ ClassDemo/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassDemo.Models
+{
+    public class Employee
+    {
+        public int empid { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string gender { get; set; }
+        public string dob { get; set; }
+        public string street1 { get; set; }
+        public string street2 { get; set; }
+        public string city { get; set; }
+        public int pin { get; set; }
+        public string dist { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string exp { get; set; }
+        public string workstate { get; set; }
+        public string workload { get; set; }
+        public string workcountry { get; set; }
+        public string address { get; set; }
+        public string enable { get; set; }
+        
+        //public string filename { get; set; }
+
+    }
+}
